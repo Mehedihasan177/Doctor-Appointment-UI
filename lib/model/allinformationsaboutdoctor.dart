4022fc99a8db3b99ed1ca21c09cf5fa54f281268
Md. Mehedi Hasan <68287045+Mehedihasan177@users.noreply.github.com>
2021-07-10 22:52:53 +0600

    Create allinformationsaboutdoctor.dart

---
+++ lib/model/allinformationsaboutdoctor.dart
@@ -0,0 +1,15 @@
+import 'package:meta/meta.dart';
+
+class DoctorINformation {
+  late final String name;
+  late final String department;
+  late final String experience;
+  late final String image;
+
+  DoctorINformation({
+    required this.name,
+    required this.department,
+    required this.experience,
+    required this.image,
+});
+}
