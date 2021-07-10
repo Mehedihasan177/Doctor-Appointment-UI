6b19fdf490c2d9bc32413000bd0dc4fbcae3d7d6
Md. Mehedi Hasan <68287045+Mehedihasan177@users.noreply.github.com>
2021-07-10 22:50:09 +0600

    Create getInformation.dart

---
+++ lib/informations/getInformation.dart
@@ -0,0 +1,31 @@
+import 'package:practice/model/model.dart';
+
+class GetInformation{
+  static final List<Diseasesinformation> infomations = [
+    Diseasesinformation(
+      name: "Dental",
+      number: "26 Doctors",
+      image: "images/teeth.png"
+    ),
+    Diseasesinformation(
+        name: "Heart",
+        number: "26 Doctors",
+        image: "images/heart.png"
+    ),
+    Diseasesinformation(
+        name: "Brain",
+        number: "26 Doctors",
+        image: "images/humanbrain.png"
+    ),
+    Diseasesinformation(
+        name: "Eye",
+        number: "26 Doctors",
+        image: "images/eye.png"
+    ),
+    Diseasesinformation(
+        name: "Bones",
+        number: "26 Doctors",
+        image: "images/bone.png"
+    ),
+  ];
+}
